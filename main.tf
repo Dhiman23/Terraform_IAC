0289a574e498884435222b5e20df3950cd3603aa
Dhiman23 <sajaldhiman68@gmail.com>
2024-11-12 01:35:05 +0530

    corrected file

--- main.tf
+++ main.tf
@@ -103,7 +103,7 @@ resource "aws_route_table" "public-rt" {
   vpc_id = aws_vpc.my-vpc.id
 
   route {
-    cidr_block = "10.0.1.0/24"
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
   tags = {
