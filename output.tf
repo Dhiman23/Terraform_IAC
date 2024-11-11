043eed159386425329a51fae0ead20815467594f
Dhiman23 <sajaldhiman68@gmail.com>
2024-11-12 02:16:55 +0530

    corrected file

--- output.tf
+++ output.tf
@@ -3,5 +3,6 @@ output "web-server-dns" {
 }
 output "db_password" {
   value       = aws_secretsmanager_secret_version.password.secret_string
+  sensitive = true
   description = "The generated database password stored in AWS Secrets Manager"
 }
